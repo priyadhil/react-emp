9bd726e1b1fbc3860148eda223555ca14707f92e
Mohanapriya <priya05mca@gmail.com>
2019-05-14 12:28:20 +0100

    initial commit

---
+++ src/App.js
@@ -0,0 +1,44 @@
+import React from "react";
+import "./App.css";
+import "./images";
+
+const App = () => <PersonList />;
+
+const PersonList = () => {
+  const people = [
+    { img: 1, name: "bob", job: "designer" },
+    { img: 2, name: "dhil", job: "developer" },
+    { img: 3, name: "rav", job: "manager" }
+  ];
+  return (
+    <section>
+      <Person person={people[0]} />
+      <Person person={people[1]}>
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
+        tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim
+        veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea
+        commodo consequat.
+      </Person>
+      <Person person={people[2]} />
+    </section>
+  );
+};
+
+const Person = props => {
+  const { img, name, job } = props.person;
+  const { children } = props;
+  const url = `images/'${img}'.jpg`;
+  // const url = "https://randomuser.me/api/portraits/thumb/men/22.jpg";
+  return (
+    <div className="person">
+      <img src={url} alt="" />
+      <div>
+        <h4>{name}</h4>
+        <h4>{job}</h4>
+        {children}
+      </div>
+    </div>
+  );
+};
+
+export default App;
